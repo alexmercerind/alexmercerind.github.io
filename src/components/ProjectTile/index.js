cfe85ff0e4474a59d0277b8705c8c6fa400a9ef1
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-10-03 19:12:37 +0530

    style: ProjectTile programming lang colors

--- src/components/ProjectTile/index.js
+++ src/components/ProjectTile/index.js
@@ -9,113 +9,174 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import clsx from "clsx";
 
 const kLanguageColors = {
-  "Dart": "#00B4AB",
-  "Python": "#3572A5",
-  "C": "#555555",
+  Dart: "#00B4AB",
+  Python: "#3572A5",
+  C: "#555555",
   "C++": "#F34B7D",
   "C#": "#178600",
-  "Java": "#B07219",
-  "JavaScript": "#F1E05A",
-  "TypeScript": "#2B7489",
-  "HTML": "#E44B23",
-  "CSS": "#563D7C",
-}
+  Java: "#B07219",
+  JavaScript: "#F1E05A",
+  TypeScript: "#2B7489",
+  HTML: "#E44B23",
+  CSS: "#563D7C",
+};
 
-export default function ProjectTile({ type, index, maintainer, repository, description, images, extraLanguages }) {
+export default function ProjectTile({
+  type,
+  index,
+  maintainer,
+  repository,
+  description,
+  images,
+  extraLanguages,
+}) {
   const [repositoryJSON, setRepositoryJSON] = useState(null);
   useEffect(async () => {
-    let response = await fetch(`https://api.github.com/repos/${maintainer}/${repository}`);
+    let response = await fetch(
+      `https://api.github.com/repos/${maintainer}/${repository}`
+    );
     let responseJSON = await response.json();
     setRepositoryJSON(responseJSON);
   }, []);
 
-  return <div data-aos={`fade-up-${index % 2 == 0 ? "right" : "left"}`} className={styles.card}>
-    <div className={styles.titleBar}>
-      <div className={styles.titleBarContent}>
-        {type == undefined ? <a href={repositoryJSON?.html_url} target={"__blank"} className={styles.title}>
-          {repository}
-        </a> : <Link className={styles.title} to={`/docs/${repository}`}>{repository}</Link>}
-        {type == undefined ? <a href={repositoryJSON?.html_url} target={"__blank"} className={clsx("material-icons", "md-36", styles.titleBarIcon)}>open_in_new</a> : <Link className={clsx("material-icons", "md-36", styles.titleBarIcon)} to={`/docs/${repository}`}>open_in_new</Link>}
+  return (
+    <div
+      data-aos={`fade-up-${index % 2 == 0 ? "right" : "left"}`}
+      className={styles.card}
+    >
+      <div className={styles.titleBar}>
+        <div className={styles.titleBarContent}>
+          {type == undefined ? (
+            <a
+              href={repositoryJSON?.html_url}
+              target={"__blank"}
+              className={styles.title}
+            >
+              {repository}
+            </a>
+          ) : (
+            <Link className={styles.title} to={`/docs/${repository}`}>
+              {repository}
+            </Link>
+          )}
+          {type == undefined ? (
+            <a
+              href={repositoryJSON?.html_url}
+              target={"__blank"}
+              className={clsx("material-icons", "md-36", styles.titleBarIcon)}
+            >
+              open_in_new
+            </a>
+          ) : (
+            <Link
+              className={clsx("material-icons", "md-36", styles.titleBarIcon)}
+              to={`/docs/${repository}`}
+            >
+              open_in_new
+            </Link>
+          )}
+        </div>
+        <div className={styles.description}>
+          {description ??
+            repositoryJSON?.description?.replace(
+              /([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g,
+              ""
+            )}
+        </div>
       </div>
-      <div className={styles.description}>
-        {description ?? repositoryJSON?.description?.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, "")}
-      </div>
-    </div>
-    <div className={styles.cardContent}>
-      {repositoryJSON !== null ? <div className={styles.iconBar}>
-        <div className={styles.iconContainer}>
-          <div className={styles.icon}>
-            <StarIcon size="small" />
-          </div>
-          <div className={styles.iconLabel}>
-            {(repositoryJSON?.stargazers_count ?? "Unknown") + " Stars"}
-          </div>
-        </div>
-        <div className={styles.iconContainer}>
-          <div className={styles.icon}>
-            <RepoForkedIcon size="small" />
-          </div>
-          <div className={styles.iconLabel}>
-            {(repositoryJSON?.forks_count ?? "Unknown") + " Forks"}
-          </div>
-        </div>
-        <div className={styles.iconContainer}>
-          <div className={styles.icon}>
-            <EyeIcon size="small" />
-          </div>
-          <div className={styles.iconLabel}>
-            {(repositoryJSON?.subscribers_count ?? "Unknown") + " Watchers"}
-          </div>
-        </div>
-      </div> : null}
-      <div className={styles.languageBar}>
-        {
-          extraLanguages.map((language) => {
-            return (
-              <div key={language} style={{ "display": "flex", "flexDirection": "row", "alignItems": "center" }}>
-                <div style={{ borderRadius: "50%", marginRight: 8, height: 16, width: 16, backgroundColor: kLanguageColors[language] }}></div>
-                <div className={styles.iconLabel}>
-                  {language}
-                </div>
+      <div className={styles.cardContent}>
+        {repositoryJSON !== null ? (
+          <div className={styles.iconBar}>
+            <div className={styles.iconContainer}>
+              <div className={styles.icon}>
+                <StarIcon size="small" />
               </div>
-            );
-          })
-        }
-        <div style={{ borderRadius: "50%", marginRight: 8, height: 16, width: 16, backgroundColor: kLanguageColors[repositoryJSON?.language] }}></div>
-        <div className={styles.iconLabel}>
-          {repositoryJSON?.language}
+              <div className={styles.iconLabel}>
+                {(repositoryJSON?.stargazers_count ?? "Unknown") + " Stars"}
+              </div>
+            </div>
+            <div className={styles.iconContainer}>
+              <div className={styles.icon}>
+                <RepoForkedIcon size="small" />
+              </div>
+              <div className={styles.iconLabel}>
+                {(repositoryJSON?.forks_count ?? "Unknown") + " Forks"}
+              </div>
+            </div>
+            <div className={styles.iconContainer}>
+              <div className={styles.icon}>
+                <EyeIcon size="small" />
+              </div>
+              <div className={styles.iconLabel}>
+                {(repositoryJSON?.subscribers_count ?? "Unknown") + " Watchers"}
+              </div>
+            </div>
+          </div>
+        ) : null}
+        <div className={styles.languageBar}>
+          {[...new Set([...extraLanguages, ...[repositoryJSON?.language]])].map(
+            (language) => {
+              return (
+                <div
+                  key={language}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      borderRadius: "50%",
+                      marginRight: 8,
+                      height: 16,
+                      width: 16,
+                      backgroundColor: kLanguageColors[language],
+                    }}
+                  ></div>
+                  <div className={styles.iconLabel}>{language}</div>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
+      {images != undefined ? <div className={styles.divider}></div> : null}
+      {images != undefined ? (
+        <Carousel
+          renderArrowPrev={(clickHandler, hasPrev, label) => {
+            return (
+              <span className="arrow-left" onClick={clickHandler}>
+                <span className={clsx("material-icons", styles.carouselIcon)}>
+                  arrow_left
+                </span>
+              </span>
+            );
+          }}
+          renderArrowNext={(clickHandler, hasNext, label) => {
+            return (
+              <span className="arrow-right" onClick={clickHandler}>
+                <span className={clsx("material-icons", styles.carouselIcon)}>
+                  arrow_right
+                </span>
+              </span>
+            );
+          }}
+          emulateTouch={false}
+          dynamicHeight={true}
+          infiniteLoop={true}
+          showIndicators={false}
+          showStatus={false}
+          showArrows={true}
+          showThumbs={false}
+        >
+          {images.map((e) => (
+            <div className={styles.image}>
+              <img loading="lazy" className={styles.imageChild} src={e}></img>
+            </div>
+          ))}
+        </Carousel>
+      ) : null}
     </div>
-    {images != undefined ? <div className={styles.divider}></div> : null}
-    {
-      images != undefined ? <Carousel
-        renderArrowPrev={(clickHandler, hasPrev, label) => {
-          return (
-            <span className="arrow-left" onClick={clickHandler}>
-              <span className={clsx("material-icons", styles.carouselIcon)}>arrow_left</span>
-            </span>
-          )
-        }}
-        renderArrowNext={(clickHandler, hasNext, label) => {
-          return (
-            <span className="arrow-right" onClick={clickHandler}>
-              <span className={clsx("material-icons", styles.carouselIcon)}>arrow_right</span>
-            </span>
-          )
-        }}
-        emulateTouch={false}
-        dynamicHeight={true}
-        infiniteLoop={true}
-        showIndicators={false}
-        showStatus={false}
-        showArrows={true}
-        showThumbs={false}
-      >
-        {
-          images.map((e) => <div className={styles.image} ><img loading="lazy" className={styles.imageChild} src={e}></img></div>)
-        }
-      </Carousel > : null
-    }
-  </div >
+  );
 }
